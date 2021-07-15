90dbcc44c1bf3739c9b58fac1c01259de22acb59
edgargv <l16140654@queretaro.tecnm.mx>
2021-07-15 11:28:25 -0500

    Carpetas

---
+++ Test Suites/FVAutorizacionesReferencia.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>FVAutorizacionesReferencia</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6ba7dd07-bcf1-4f4c-868c-9e3bd1cd8fd1</testSuiteGuid>
+</TestSuiteEntity>
